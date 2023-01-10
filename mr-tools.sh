1643ba5dd4162afa717445c19dd1067948a410c7
Imran Shaikh <51431951+Imran407704@users.noreply.github.com>
2023-01-10 12:12:09 +0530

    Update mr-tools.sh

--- mr-tools.sh
+++ mr-tools.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-printf "Installing Pre-requisites" \n\n
+printf "Installing Pre-requisites" \n
 
 apt-get install python3;
 apt install python3-pip;
 apt-get install golang;
 
-printf "Installing Tools" \n\n
+printf "Installing Tools" \n
 
 go install -v github.com/tomnomnom/assetfinder@latest;
 go install -v github.com/tomnomnom/waybackurls@latest;
@@ -21,7 +21,7 @@ go install -v github.com/edoardottt/cariddi/cmd/cariddi@latest;
 go install -v github.com/003random/getJS@latest;
 go install -v github.com/mlcsec/headi@latest;
 
-printf "Installing hakluke tools" \n\n
+printf "Installing hakluke tools" \n
 
 go install -v github.com/hakluke/hakrawler@latest;
 go install -v github.com/hakluke/hakrevdns@latest;
@@ -29,7 +29,7 @@ go install -v github.com/hakluke/hakcheckurl@latest;
 go install -v github.com/hakluke/haktrails@latest;
 go install -v github.com/tomnomnom/anew;
 
-printf "Installing Project Discovery Tools" \n\n
+printf "Installing Project Discovery Tools" \n
 
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest;
@@ -42,12 +42,12 @@ go install -v github.com/projectdiscovery/katana/cmd/katana@latest;
 go install -v github.com/projectdiscovery/uncover/cmd/uncover@latest;
 go install -v github.com/projectdiscovery/dnsprobe;
 
-printf "Copying Binary file to usr/local/bin" \n\n
+printf "Copying Binary file to usr/local/bin" \n
 cp $HOME/go/bin/* /usr/local/bin
 
-printf "Installing PIP tools" \n\n
+printf "Installing PIP tools" \n
 
 pip3 install arjun;
 pip install xsrfprobe;
 
-printf "Done" \n\n
+printf "Done" \n
